2bfc373480c39f8e875e32024da4a571d22b6b6f
Eriko Dott <dotte@oregonstate.edu>
2021-07-20 15:27:16 -0700

    modified home main page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,134 @@
 <template>
   <div class="home">
-    Home goes here
+    <h1 class ="title">7 Wonders Of Oregon</h1>
+    <div class=" containers">
+        <div>
+            <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sed laoreet eros. Maecenas non semper
+                neque.
+                Fusce lobortis urna volutpat ligula consequat gravida. Suspendisse auctor dui eu libero blandit gravida.
+                Morbi aliquam nunc nisi, et posuere nisl sollicitudin ac.
+                Vestibulum congue nisi ac quam imperdiet, non malesuada ante sodales. Cras ultrices quis augue vitae
+                ultricies.
+            </p>
+        </div>
+        
+        <ul class="flex-container">
+            <li class="flex-item">
+                <div class="card" style="width: 18rem;">
+                    <router-link class="link" to="/location/Columbia River Gorge">
+                    <img src="https://lh3.googleusercontent.com/TDx0wXBw9_mp9ozIrQFcHAQUVCt22tjgq78e0tvtsuw3Z04EYXNR0WCax3V4ZAZDJfF86hqC91VN92u49Un1kpqS8POJ9cvDfR3qy8Cst29h6u2EcYWsm1KsBErPO2zVdJumFUIR5N7cNquEUiu9BNuSGJraYMuNHkkX-Yx3Gro46UW1AaGPV7NRd2QCeTZ4LcspuMzyHitwIBbTAkcoqcIFP1fFaUfCgwwNr5-xV3Ag4SJi2lbKD9Pqkx0vWzWUhM_gft7dGEgk1DwM2j7Wp95goM7hBfkC3NoRjXw3geFGm7gSi-zKcRTIhiv1XpFEv0nZWHkeyDApM2w2-6S7_X8-S1dEqrsHbukzeHs4PAeAGBUpp091aIq5kZXChzFqmRzN_RTDIC3WeFg6OLkQhvn21DxfUrKIht3V79MoavFFP-PQBjyYjHTfu3cOZCDDvsR7gZ9q7eZFbuWzGCsSRt3owQge9d6dE6NI3z4wNs_oLT087s1R1aKCQzMjYBIuRoelv26TCE4Rl9a8CzZkBO8PIA5_MYm5Mz4vsttR8saGLpeYyrTATkukJthTc2EHnKNyQL5HKlr-yBfCBF_bp-D2MjRM23snU4badRG4eThiYmODzKhW6FzY3H5qOmQ1oM8YdiKGd0UGwNeueP1izbeD20GkN7_UEJYPa8pqwWZidCQcT0xDThaicj2JJvPJS6Ieq3U8Pfc6bppyOhRuP9Bj=w960-h540-no?authuser=0" class="card-img-top" alt="Columbia River Gorge">
+                    <div class="card-body">
+                        <p class="card-text">Columbia River Gorge</p>
+                    </div>
+                    </router-link>
+                </div>
+            </li>
+            <li class="flex-item">
+                <div class="card" style="width: 18rem;">
+                    <router-link class="link" to="/location/Crater Lake">
+                    <img src="https://lh3.googleusercontent.com/vxrW-1QHeBS2TJDveNQI58I8VEdDK9FoOQ4uOEXr_3tWk2GO7TAQttW0cIeol2i5Znx_nL6yPopkFspSxWbX11gvrfQ097RtOwq0IKQ1nImP-Tp4SK2EzbjS4aNWJdbv28tCJFERzciNNdN1Lpa_HgfyfJ-R0P0o2vZhHE7KTouOh1ywsBY63aAywy927q69y5E3TF49kgbTmcIB0ta29wEzMP6wVeAjsslfVpPHdDuhoY7pimRB7bfZPUPadedZobxPEQwwbQJp7IiXqHLrpLWBuIjiBSBpszd471KQl3r9oVfCPH9y48iB_rR6hDMGk7pSBkU0sGM4DiY5tQe4DzX_lIEJQAD4rb4qti8WzHfKp__bpRhzWrOlELe2TpAtTBGQv1bc9zwAzHFldh3S1VPKDJxL7DWL8QXfqQK94u3eAF_YQUIm2ydGFOLcphzUdtrfZZ_72rgjKeizSwruLkPv75ygSBzW1hJEsxpljBPDJ01rm_peRcQhxT4EM25XVz8-akqZOAmAdNbondmlkJs8gA9Tsv1X8OIxAr2UQK4XG4zBOyzpZhVQKHQ-TPnjeLDKj0PhZtUGBVfnS9EjOZNbSdL1U3FbGFiPLPZSj_BZA_8D6w9N5bKKLT_55JCP7Ue5AG8ApHfVf1v-5ctjZa0fLfZ3S4DhIrNEvL-9cAmHbcvaZx7CDf2Y4T5xF9Ckwbp4G0V_6OSmtWKD8rOT6qDq=w960-h540-no?authuser=0" class="card-img-top" alt="Crater Lake">
+                    <div class="card-body">
+                        <p class="card-text">Crater Lake</p>
+                    </div>
+                    </router-link>
+                </div>
+            </li>
+            <li class="flex-item">
+                <div class="card" style="width: 18rem;">
+                    <router-link class="link" to="/location/Painted Hills">
+                    <img src="https://lh3.googleusercontent.com/olCsisxWA8GeqXwIjddoYALtnRMD0IujC8yfdjax_LNQSC_Wugsu3jltr36vdVZcFJK2jCSWBzrO6LFmgYg5_nfemrUDhC8NdN6LOFtQCoWUeldP3Bw9FyWCZFlS3BiIdNgiBHzVBvx1Wx_7HueZnaQ11CUzjYwf4S3_E3ifVo6GwQ-X3JUWuPw_RTZCTBzJ-e-hi5P1ywdySXRQ0uXX1Ii54UmO-6aXwd5iaS62ANDnyvAG_Bd2GZEIhuyoG5d0NNem_t37DxhdWcDBm5TnZJQP67gLt9Qla4cu8ep3ghPryJmAMhGHSFh6JRpWMBEmU8iFKEN4p6TiGcYiKJrWGiUbFQF-vqsA0Tu1_BmbMeJ9bebtEwYsa60_fNqgOe5eBSYEzFUhdJvn1XhuX2u3TB9Yc7JIqZO0jqjPYDpK4Dgr2mgspJatm6tXGubr3N8HE2YS76uNCkh5joIN8qqerlea0qHn80fsmwC2JVtCpwRlJqAJbRHAWn7R-XDF5SxY7lkYw07BWPqoYdvH15qL92vRPQkB4RfOu9qrFerBcYOpx7tEwIaChm-vjJKnqw35TM9IBF-yfMb7z5b_idUOO82zvdqiO8issTjwNhFSj2LQ0R_7RRMPQzSO8tXR4bA7_dRur6NPuQtJlOTMyVfS4r6Lq-zMDr8yVpNutfT4J9E0Imn8mvJOZLhYDm9SWEVZs7DioKCpbrpyMW8Ro9pa8Fr9=w960-h540-no?authuser=0" class="card-img-top" alt="Painted Hills">
+                    <div class="card-body">
+                        <p class="card-text">Painted Hills</p>
+                    </div>
+                    </router-link>
+                </div>
+            </li>
+            <li class="flex-item">
+                <div class="card" style="width: 18rem;">
+                    <router-link class="link" to="/location/Oregon Coast">
+                    <img src="https://lh3.googleusercontent.com/fg_ThEp2DPRMkxInIprSzDst9PSyV0El9QHH4Ao7LNeFE8eFRf5znNAScFa-m7hmIyIKauhwdTlJawwqY-lwBAQzAACFNAoBUxu58vF628QsZlWC2yDW9AerjIq6CoHB3IInkiZnJMj2sVgrtRfiDnRh3CGsqG-JI-0LCE5lbOzJ5M0p8yG9LEx2lKRBsaumfU1pqRsRH3Qnd63QwDloQ5VHymt0FQo4IIS4up1BuAFon9P1uhfthc2Au6LqiMAxLCAqgJllytuN27h69rBAy27_nncmqtShSNc3SiNKL788-M4NVdaXNaQJGtarWvbtS_u5Hobs7LVeBowM3yP8A-Xv9VCKrVa5Ryhi4J752-x4aiLK2qmR4JB45WFz4aJUxaR957VB2sIUR69aPS1gRG-IatWJsOaGGDDm2no-FgCcG73NOqkiqQuvT8Refppmbxo0V-J8imuUVSEMvkZRyejyj_IuXVkexP7DuVvR8g4afLdW5bnJtEPnI5XfopZQEI9ZcW5EQECG9r23730hVsYWRJvqsq7fRwMnBfBcqQBXtSQBqxaIFr1R3NmuOznW2fCyq2XciOufDHS76hBilKLa6lKDfWLhy0RNsEX8AkMxR0BBfA-OOP3DriuVqcrPw_zxnN7YlElRQmFstijuucAe7ZLrbM0De7Tejs9ThpoKok9UqacGhDXBkcfWwO6LhItt6UeNPXe9D-35gLi5kGPK=w960-h540-no?authuser=0" class="card-img-top" alt="Oregon Coast">
+                    <div class="card-body">
+                        <p class="card-text">Oregon Coast</p>
+                    </div>
+                    </router-link>
+                </div>
+            </li>
+        </ul>
+        <ul class="flex-container">
+            <li class="flex-item">
+                <div class="card" style="width: 18rem;">
+                    <router-link class="link" to="/location/Smith Rock">
+                    <img src="https://lh3.googleusercontent.com/pw/AM-JKLVGplL8-uI-Z-W-57jttQVv2uWl6A-PMItKNN9qyM8CdR8_b9Ka_iGfxa3w_EbBZe8K3ftY8glJX55B_o8toMVERDiJ9QXDAjxl0iDEWASE9qHG4Uo8ELo_F9xPpf7WV96wPWTr26t6VXpTVkDuZ2C_=w960-h720-no?authuser=0" class="card-img-top" alt="Smith Rock">
+                    <div class="card-body">
+                        <p class="card-text">Smith Rock</p>
+                    </div>
+                    </router-link>
+                </div>
+            </li>
+            <li class="flex-item">
+                <div class="card" style="width: 18rem;">
+                    <router-link class="link" to="/location/The Wallowas">
+                    <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/01/80/09/8b/wallowa-lake-state-park.jpg?w=600&h=-1&s=1" class="card-img-top" alt="The Wallowas">
+                    <div class="card-body">
+                        <p class="card-text">The Wallowas</p>
+                    </div>
+                    </router-link>
+                </div>
+            </li>
+            <li class="flex-item">
+                <div class="card" style="width: 18rem;">
+                    <router-link class="link" to="/location/Mt. Hood">
+                    <img src="https://lh3.googleusercontent.com/XYuUdi1w17DKKZrt3lGG-uy50GW11Cmq9MExec8JlsTNEsDOIcM3UPSR9D-I4514hG1mLnmM5lcwZdY-JEAh6WWaIrP3yLiAATvyMVV4s6xcnjvDrKgJnh6rhhOR9zD4HLS8bJMuQQTA4yqN7ESP6NtuvxlQXeadHuSX6rIVU1B7p2a5Ph4HItrHkuE2u3nnd5l_Myzc5n12IODeHA67jfVf8W8JnWjzTrrYl_tViEyV9MFt9Kt0JLxdex2DV8FrcW3CDM8UDjbdV60OT908tq8vjvqljTHSRmYQNHDK8OekNdkjyOjh0mP9sAGRH4ISfvtxSmPM4ntmJ9cjG9NR-z2bemrKTx2F4yeNT0T4o0uGWBv0hMyOR_3yFUqL-VLMs_mL2c-YSOr1fAfsfbh_9Zc2fm25CT6uFoj_1rHG4PchSadWyS32MMaua-BQCxQghCHl5bxJJ8VCDWLU17XrTo7gOCTtZ_V2SB-cBPWXz1Zd8XjD8cHgqhzxfEMkn_NAhjaDYN7NbtMM9HsiUV6xt8W8Njp5abxCXJ06iCa_m7EhHi3TGj6PjxFdTaa2QvyPSlrIh_s61ifsALWfhowfkXLQZbDXuCzcNAKREXfbJUIOdYjRYZnla4r7OJLrRJXCErfEW6mEzegvlmoaL88D1BmM26rFXByc_Ehzux_YW4QCZ2lS0X8cdEIJMXc3w30eR0UOnjD3A_5UYfIHkqYWFZfc=w960-h540-no?authuser=0" class="card-img-top" alt="Mt. Hood">
+                    <div class="card-body">
+                        <p class="card-text">Mt. Hood</p>
+                    </div>
+                    </router-link>
+                </div>
+            </li>
+        </ul>       
+      <div>
+    </div>
   </div>
+  <!--containers-->
+</div>
 </template>
 
 <script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home'
+  name: 'Home',
+  data(){
+    return{
+      name: this.$route.params.locname
+    } 
+  }
 }
 </script>
+
+<style>
+
+.title{
+  padding: 0;
+  margin: 50px;
+}
+
+.text{
+  padding: 0;
+  margin: 80px;
+}
+.flex-container {
+  /* We first create a flex layout context */
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-around;
+  padding: 0;
+  margin: 10px;
+  list-style: none;
+}
+
+.flex-item {
+  margin: 10px;
+}
+</style>
