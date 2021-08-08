7bb97d60e5ccaacab23bc8a4c9d2efbada9a9770
Eriko Dott <dotte@oregonstate.edu>
2021-08-08 13:08:26 -0700

    added summry api

--- src/components/Summary.vue
+++ src/components/Summary.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <p>{{ content }}</p>
+        <p>{{ summary }}</p>
         
     </div>
   
@@ -8,28 +8,56 @@
 
 <script>
 
-import locationData from '@/locationData.json'
-
+import locationData from "@/locationData.json";
 
 export default {
-    props: {
+  devServer: {
+    proxy: "http://localhost:8080",
+    host: "localhost",
+  },
+  props: {
     locName: {
       type: String,
       required: true,
       default: "none"
     },
   },
-    data(){
-        var summary = null;
-        locationData.data.forEach(x => {
-            
-            if(x.name === this.locName) summary = x.summary
-        });
-        
-        return{
-            content: summary,
+  data() {
+    return {
+      summary: null
+    };
+  },
+  created() {
+    this.fetchData();
+  },
+  watch: {
+    $route: "fetchData",
+  },
+  methods: {
+    fetchData() {
+      var self = this;
+      var url =null
+      self.name = this.locName
+
+
+      locationData.data.forEach((x) => {
+        if (x.name === self.name) {
+          url = "http://localhost:3495/wikiscraper/" + x.wiki;
         }
-    
-  }
-}
+      });
+      console.log(url)
+      fetch(url)
+        .then(function (resp) {
+          return resp.text();
+        }) 
+        .then(function (obj) {
+          console.log("test",obj)
+          self.summary = obj
+
+        });
+    },
+  },
+};
+
+
 </script>
